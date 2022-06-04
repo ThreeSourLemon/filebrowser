eec19b3e5d1a8f9dbf2163bff5f168d407aa6d5c
ThreeSourLemon <43909683+ThreeSourLemon@users.noreply.github.com>
2022-06-04 18:34:39 +0800

    Update Filebrowser.sh

--- Filebrowser.sh
+++ Filebrowser.sh
@@ -89,8 +89,10 @@ Download_filebrowser(){
 	
 	if [[ $bit == "i386" || $bit == "i686" ]]; then
 	filebrowser="linux-386-filebrowser.tar.gz"
-    elif [[ $bit == "x86_64" ]]; then
+    	elif [[ $bit == "x86_64" ]]; then
 	filebrowser="linux-386-filebrowser.tar.gz"
+	elif [[ $bit == "aarch64" ]]; then
+	filebrowser="linux-arm64-filebrowser.tar.gz"
 	else
 		echo -e "${Error_font_prefix}[错误]${Font_suffix} 不支持 ${bit} !" && exit 1
 	fi
@@ -112,14 +114,14 @@ Download_filebrowser(){
 Service_filebrowser(){
 	if [[ ! -e /etc/init.d/filebrowser ]]; then
 	if [[ ${release} = "centos" ]]; then
-	    if ! wget --no-check-certificate https://raw.githubusercontent.com/vinewx/script/master/requirement/filebrowser_centos -O /etc/init.d/filebrowser; then
+	    if ! wget --no-check-certificate https://raw.githubusercontent.com/ThreeSourLemon/filebrowser/master/requirement/filebrowser_centos -O /etc/init.d/filebrowser; then
 			echo -e "${Error_font_prefix}[错误]${Font_suffix} Filebrowser服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/filebrowser
 		chkconfig --add filebrowser
 		chkconfig filebrowser on
 	else
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/vinewx/script/master/requirement/filebrowser_debian -O /etc/init.d/filebrowser; then
+		if ! wget --no-check-certificate https://raw.githubusercontent.com/ThreeSourLemon/filebrowser/master/requirement/filebrowser_debian -O /etc/init.d/filebrowser; then
 			echo -e "${Error_font_prefix}[错误]${Font_suffix} Filebrowser服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/filebrowser
@@ -196,6 +198,8 @@ while :; do
 	echo -e "${Info_font_prefix}[Author]${Font_suffix}: vinewx"
 	echo
 	echo -e "${Info_font_prefix}[github]${Font_suffix}: https://github.com/vinewx/script"
+	echo
+	echo -e "${Info_font_prefix}........... 增加arm64支持 ...........${Font_suffix}"
 	echo
 	echo " 1. 安装"
 	echo
